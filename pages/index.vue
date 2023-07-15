389cfb7669876d8d15d710e3b56701158d5e2079
aalekseenko <a.alekseenko@amarkets.com>
2023-07-15 17:10:50 +0300

    feat: add mеta tags and other elements to the HTML head tag

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,16 @@
 <script>
 import homes from '~/data/homes'
 export default {
+  head(){
+    return {
+      title: 'Homepage',
+      meta: [{
+        name: 'description',
+        content: 'This is a homepage!',
+        hid: 'description'
+      }]
+    }
+  },
   data(){
     return {
       homes: homes.slice(0,3)
