69efbbfe8f4649c3eb50584d3a3f78db07b4201f
aalekseenko <a.alekseenko@amarkets.com>
2023-07-16 22:25:56 +0300

    feat: add handling errors in dataApi

--- plugins/dataApi.js
+++ plugins/dataApi.js
@@ -1,15 +1,37 @@
 export default function(context, inject){
   const appId = '9OEU15A0X8'
   const apiKey = '4d88ff4a76323a2aaf9b69958a2d5f83'
+  const headers = {
+    'X-Algolia-API-Key': apiKey,
+    'X-Algolia-Application-Id': appId,
+  }
+
+  const unWrap = async (response) => {
+    const json = await response.json()
+    const { ok, status, statusText } = response
+    return {
+      json,
+      ok,
+      status,
+      statusText,
+    }
+  }
+
+  const getErrorResponse = (error) => {
+    return {
+      ok: false,
+      status: 500,
+      statusText: error.message,
+      json: {}
+    }
+  }
 
   const getHome = async (homeId) => {
-    const response = await fetch(`https://${appId}-dsn.algolia.net/1/indexes/homes/${homeId}`, {
-      headers:{
-        'X-Algolia-API-Key': apiKey,
-        'X-Algolia-Application-Id': appId,
-      }
-    })
-    return await response.json()
+    try {
+      return unWrap(await fetch(`https://${appId}-dsn.algolia.net/1/indexes/homes/${homeId}`, { headers }))
+    } catch(error){
+      return getErrorResponse(error)
+    }
   }
 
   inject('dataApi', {
